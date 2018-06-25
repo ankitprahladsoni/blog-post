20e6bb3fbaeec7deefcc158107e7a2e34d9a4a7e
ankitprahladsoni <ankitprahladsoni@gmail.com>
2018-06-25 03:04:57 -0400

    Added Article buttons

--- src/components/Article/Article.vue
+++ src/components/Article/Article.vue
@@ -1,6 +1,6 @@
 <template>
         <div class="columns">
-                <div class="column is-2">Buttons Here</div>
+                <div class="column is-2"><article-buttons></article-buttons></div>
                 <div class="column is-8"><article-content @scroll="scrollMethod"></article-content> </div>
                 <div class="column is-2"><article-menu :item-to-highlight="highlightItem"></article-menu></div>
         </div>
@@ -10,11 +10,13 @@
 import Vue from 'vue';
 import ArticleMenu from './ArticleMenu.vue';
 import ArticleContent from './ArticleContent.vue';
+import ArticleButtons from './ArticleButtons.vue';
 
 export default Vue.extend({
   components: {
     ArticleMenu,
     ArticleContent,
+    ArticleButtons,
   },
   data() {
     return {
---
+++ src/components/Article/ArticleButtons.vue
@@ -0,0 +1,15 @@
+<template>
+     <section class="sticky" id="article-buttons">
+        <i class="fab fa-github-alt"></i>
+    </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({});
+</script>
+
+<style lang="scss" scoped>
+</style>
+
+
