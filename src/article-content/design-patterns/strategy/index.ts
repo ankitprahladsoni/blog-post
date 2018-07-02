73f2108f3f40b255a70282da5d6d043e0e0ecebc
ankitprahladsoni <ankitprahladsoni@gmail.com>
2018-07-02 00:35:12 -0400

    require to import

--- src/article-content/design-patterns/strategy/index.ts
+++ src/article-content/design-patterns/strategy/index.ts
@@ -1,9 +1,9 @@
+import what from './what-is-it';
+import problem from './problem-statement';
+import soln from './the-solution';
+import otherExp from './other-examples';
+import conclusion from './conclusion';
+
 export function getArticle() {
-  const article = [];
-  article.push(require('./what-is-it').default);
-  article.push(require('./problem-statement').default);
-  article.push(require('./the-solution').default);
-  article.push(require('./other-examples').default);
-  article.push(require('./conclusion').default);
-  return article;
+  return [what, problem, soln, otherExp, conclusion];
 }
